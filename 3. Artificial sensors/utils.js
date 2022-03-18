e84f82e2e0cde324aac02df07cfb1f3e6cf652f4
Radu Mariescu-Istodor <gniziemazity@gmail.com>
2022-03-18 15:04:01 +0200

    Add files via upload
    Lecture 3 code

---
+++ 3. Artificial sensors/utils.js
@@ -0,0 +1,23 @@
+function lerp(A,B,t){
+    return A+(B-A)*t;
+}
+
+function getIntersection(A,B,C,D){ 
+    const tTop=(D.x-C.x)*(A.y-C.y)-(D.y-C.y)*(A.x-C.x);
+    const uTop=(C.y-A.y)*(A.x-B.x)-(C.x-A.x)*(A.y-B.y);
+    const bottom=(D.y-C.y)*(B.x-A.x)-(D.x-C.x)*(B.y-A.y);
+    
+    if(bottom!=0){
+        const t=tTop/bottom;
+        const u=uTop/bottom;
+        if(t>=0 && t<=1 && u>=0 && u<=1){
+            return {
+                x:lerp(A.x,B.x,t),
+                y:lerp(A.y,B.y,t),
+                offset:t
+            }
+        }
+    }
+
+    return null;
+}
