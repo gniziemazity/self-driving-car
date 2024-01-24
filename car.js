69c5817314c766c2024a7343302b53d760b7ff28
Juan Dager <68037638+pekingPow@users.noreply.github.com>
2024-01-23 22:12:18 -0500

    Adds controls for car

--- car.js
+++ car.js
@@ -4,6 +4,8 @@ class Car {
     this.y = y;
     this.width = width;
     this.height = height;
+
+    this.controls = new Controls();
   }
 
   draw(ctx) {
---
+++ controls.js
@@ -0,0 +1,48 @@
+class Controls {
+  constructor() {
+    this.forward = false;
+    this.left = false;
+    this.right = false;
+    this.reverse = false;
+
+    this.#addKeyboardListeners();
+  }
+
+  #addKeyboardListeners() {
+    document.onkeydown = (event) => {
+      switch (event.key) {
+        case "ArrowLeft":
+          this.left = true;
+          break;
+        case "ArrowRight":
+          this.right = true;
+          break;
+        case "ArrowUp":
+          this.forward = true;
+          break;
+        case "ArrowDown":
+          this.reverse = true;
+          break;
+      }
+      console.table(this);
+    };
+
+    document.onkeyup = (event) => {
+      switch (event.key) {
+        case "ArrowLeft":
+          this.left = false;
+          break;
+        case "ArrowRight":
+          this.right = false;
+          break;
+        case "ArrowUp":
+          this.forward = false;
+          break;
+        case "ArrowDown":
+          this.reverse = false;
+          break;
+      }
+      console.table(this);
+    };
+  }
+}
